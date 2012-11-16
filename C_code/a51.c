35ea6e00b9319ef37854af78ad07d8f645f950f8
rickyyy <r.bortolameotti@gmail.com>
2012-11-16 15:00:15 +0100

    Error a51 Fixed

--- C_code/a51.c
+++ C_code/a51.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-int size_lsfr_1 = 19;
-int size_lsfr_2 = 22;
-int size_lsfr_3 = 23;
+#define dim_v1 19
+#define dim_v2 22
+#define dim_v3 23
 
 char key_length = 64;
 char frame_vector_length = 22;
 
 
-char lsfr_1[size_lsfr_1] = {};
-char lsfr_2[size_lsfr_2] = {};
-char lsfr_3[size_lsfr_3] = {};
+char lsfr_1[dim_v1];
+char lsfr_2[dim_v2];
+char lsfr_3[dim_v3];
 
 char sync_1 = 8;
 char sync_2 = 10;
@@ -20,15 +20,15 @@ char sync_3 = 10;
 
 char sync_vector[3] = {0,0,0};
 
-int taps_1[4] = {13,16,17,18};
-int taps_2[2] = {20,21};
-int taps_3[4] = {7,20,21,22};
+char taps_1[4] = {13,16,17,18};
+char taps_2[2] = {20,21};
+char taps_3[4] = {7,20,21,22};
 
 
 /*
  *  Step1 - of A5/1 - Initialization of vectors
  */
-void initialize (char *vector, int size){
+void initialize_vectors (char *vector, int size){
 	int i;
 
 	for (i=0; i < size; i++){
@@ -46,19 +46,20 @@ int xor (int x, int y){
  * Xor statement between taps (starting from the end of the array)
  * how is it possible to see from http://it.wikipedia.org/w/index.php?title=File:A5-1_GSM_cipher.svg&page=1
  */
-int tap_xor_iteration (char* lsfr_vector, char* tap_array, int array_size) {
-	int result = 0;
+char tap_xor_iteration (char* lsfr_vector, char* tap_array, int array_size) {
+	char result = 0;
 	char x;
 	char y;
 	for ( ; array_size >= 0 ; array_size--){
 		x = tap_array[array_size];
-		y = lsfr_vector[x];
+		y = lsfr_vector[(int)x];
 		result = xor(result, y);
 	}
 	return result;
 }
 
 void shift_LSFR( char state[], int state_size, char res ) {
+	int j;
 	for (j = state_size - 1; j > 0 ; j--) {
 		state[j] = state[j - 1];
 	}
@@ -71,15 +72,15 @@ void step_2 (char *key){
 	char res2;
 	char res3;
 	for (i=0; i < key_length; i++){
-		res1 = tap_xor_iteration(lsfr_1, taps_1);	//tap iteration = xor between taps within the vector
-		res1 = xor(res,key[i]);						//xor with the current bit value of the key
-		shift_lsfr(lsfr_1, size_lsfr_1, res1);		//shift the vector and put in the result
-		res2 = tap_xor_iteration(lsfr_2, taps_2);
-		res2 = xor(res,key[i]);
-		shift_lsfr(lsfr_2, size_lsfr_2, res2);
-		res3 = tap_xor_iteration(lsfr_3, taps_3);
-		res3 = xor(res,key[i]);
-		shift_lsfr(lsfr_3, size_lsfr_3, res3);
+		res1 = tap_xor_iteration(lsfr_1, taps_1, dim_v1);	//tap iteration = xor between taps within the vector
+		res1 = xor(res1,key[i]);						//xor with the current bit value of the key
+		shift_LSFR(lsfr_1, dim_v1, res1);		//shift the vector and put in the result
+		res2 = tap_xor_iteration(lsfr_2, taps_2, dim_v2);
+		res2 = xor(res2,key[i]);
+		shift_LSFR(lsfr_2, dim_v2, res2);
+		res3 = tap_xor_iteration(lsfr_3, taps_3, dim_v3);
+		res3 = xor(res3,key[i]);
+		shift_LSFR(lsfr_3, dim_v3, res3);
 	}
 }
 
@@ -92,15 +93,15 @@ void step_3 (char *frame_vector){
 	int i;
 	char res1, res2, res3;
 	for (i=0; i < frame_vector_length; i++){
-		res1 = tap_xor_iteration(lsfr_1, taps_1);	//tap iteration = xor between taps within the vector
-		res1= xor(res,frame_vector[i]);				//xor with the current bit value of the frame_vector
-		shift_lsfr(lsfr_1, size_lsfr_1, res1);		//shift the vector and put in the result
-		res2 = tap_xor_iteration(lsfr_2, taps_2);
-		res2 = xor(res,frame_vector[i]);
-		shift_lsfr(lsfr_2, size_lsfr_2, res2);
-		res3 = tap_xor_iteration(lsfr_3, taps_3);
-		res3 = xor(res,frame_vector[i]);
-		shift_lsfr(lsfr_3, size_lsfr_3, res3);
+		res1 = tap_xor_iteration(lsfr_1, taps_1, dim_v1);	//tap iteration = xor between taps within the vector
+		res1= xor(res1,frame_vector[i]);				//xor with the current bit value of the frame_vector
+		shift_LSFR(lsfr_1, dim_v1, res1);		//shift the vector and put in the result
+		res2 = tap_xor_iteration(lsfr_2, taps_2, dim_v2);
+		res2 = xor(res2,frame_vector[i]);
+		shift_LSFR(lsfr_2, dim_v2, res2);
+		res3 = tap_xor_iteration(lsfr_3, taps_3, dim_v3);
+		res3 = xor(res3,frame_vector[i]);
+		shift_LSFR(lsfr_3, dim_v3, res3);
 	}
 
 }
@@ -109,7 +110,7 @@ void step_3 (char *frame_vector){
 /*
  * get the majority of bits -> if the majority it's 1 then i'll return 1, 0 otherwise
  */
-char get_majority_bit(char b1, char b2, char b3) {
+char get_majority_bit_a51(char b1, char b2, char b3) {
 	char sum = b1 + b2 + b3;
 	if (sum > 1)
 		return 1;
@@ -130,8 +131,15 @@ void step_4 (char* lsfr_vec1, char* lsfr_vec2, char* lsfr_vec3){
 	int i;
 	int maj_bit = 0;
 	for (i=0; i<100; i++){
-		maj_bit = get_majority_bit(sync_1, size_lsfr_2, sync_3);
-		if(lsfr_vec1[sync_1]==maj_bit){
+		maj_bit = get_majority_bit_a51(sync_1, dim_v2, sync_3);
+		if(lsfr_vec1[sync_1-1]==maj_bit){
+
+		}
+		if(lsfr_vec2[sync_2-1]==maj_bit){
+
+		}
+		if(lsfr_vec3[sync_3-1]==maj_bit){
+
 		}
 	}
 }
